c572345560c780fec8ccf7477b90d51247ae8f4d
Rupesh Varshney <146447594+rupeshv2121@users.noreply.github.com>
2025-01-10 13:25:56 +0530

    Added Cursor animation

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ tl.from(".nav-links li", {
     duration: 1,
     y: -30
 })
+
+const cursor = document.querySelector("#cursor");
+const body = document.querySelector("body")
+body.addEventListener("mousemove", function (e) {
+    gsap.to(cursor, {
+        x: e.x,
+        y: e.y,
+        duration: 0.5
+    })
+})
